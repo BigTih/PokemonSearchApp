e6e3d8d08b6495e69896aaf948d3cc83fa7b9e1f
Tim Glavan <glavantim@gmail.com>
2023-10-30 00:18:16 -0500

    Added some stats and tiny bit of format

--- script.js
+++ script.js
@@ -13,23 +13,52 @@ function fetchPokemon(query) {
     fetch(API_ENDPOINT)
         .then(response => response.json())
         .then(data => {
-            displayPokemon(data.abilities);
+            displayPokemon(data);
         })
         .catch(error => {
-            console.error('There was an error fetching the books:', error);
+            displayPokemon(null);
+            console.error('There was an error fetching the pokemon:', error);
         });
 }
 
 function displayPokemon(pokemon) {
     resultsList.innerHTML = ''; // clear previous results
-    if (pokemon && pokemon.length > 0) {
-        pokemon.forEach(pok => {
-            const title = pok.ability.name;
+    if(pokemon)
+    {
+        const nameData = pokemon.name;
+        const first = nameData.charAt(0)
+        const cap = first.toUpperCase()
+        const ame = nameData.slice(1)
+        const name = cap + ame
+        const listName = document.createElement('li');
+        listName.textContent = "Name: "+name;
+        resultsList.appendChild(listName);
+
+        const xp = pokemon.base_experience;
+        const listXP = document.createElement('li');
+        listXP.textContent = "Base Experience: "+xp;
+        resultsList.appendChild(listXP);
+
+        const height = pokemon.height;
+        const listHeight = document.createElement('li');
+        listHeight.textContent = "Height: "+height;
+        resultsList.appendChild(listHeight);
+
+        const weight = pokemon.weight;
+        const listWeight = document.createElement('li');
+        listWeight.textContent = "Weight: "+weight;
+        resultsList.appendChild(listWeight);
+    }
+    //if (pokemon && pokemon.length > 0) {
+
+        //pokemon.forEach(pok => {
+            /*const name = pokemon.name;
             const listItem = document.createElement('li');
-            listItem.textContent = title;
-            resultsList.appendChild(listItem);
-        });
-    } else {
+            listItem.textContent = name;
+            resultsList.appendChild(listItem);*/
+        //});
+    else 
+    {
         const listItem = document.createElement('li');
         listItem.textContent = 'No pokemon found.';
         resultsList.appendChild(listItem);
